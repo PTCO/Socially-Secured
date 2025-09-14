1ada76bd4e940a222ec13a9f3b1143df051fdcf9
ptco.dev@gmail.com <ptco.dev@gmail.com>
2025-09-13 19:18:10 -0500

    Updated csproj

--- app/app/obj/Debug/net9.0/app.AssemblyInfo.cs
+++ app/app/obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0e9079c74b88377059579b34b9f57686fb7c69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e174529f5ebceffee76ac08f3b2773f4bdb159a")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
